e0936ca2d7ff8e339a8aab95fc7850d1238ea300
Paulo Coto <pbarca@hotmail.com>
2021-02-14 17:48:54 +0000

    alteramos cliente para login

--- user.py
+++ user.py
@@ -9,7 +9,7 @@ class User:
 
     def reset(self):
         self.id = None
-        self.cliente = ''
+        self.login = ''
         self.email = ''
         self.password = ''
         self.nif = ''
