5854874003ea627a0c1785845d6488acc86498a5
Paulo Coto <pbarca@hotmail.com>
2021-02-14 17:44:54 +0000

    Initial commit

---
+++ db.py
@@ -0,0 +1,7 @@
+class Database:
+
+    def __init__(self):
+        self.Host = 'ec2-52-209-134-160.eu-west-1.compute.amazonaws.com'
+        self.Database = 'dav5i22li2jrrj'
+        self.User = 'itmuvbfzedxqkj'
+        self.Password = 'cdbd5af7afc2a1330589b97900c5e29dbf9e334a666667fb5a7ab5f42e349f00'
