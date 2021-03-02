54cfcc28dd785e71341fa8680efe1e462eb2798e
Paulo Coto <pbarca@hotmail.com>
2021-03-02 14:01:18 +0000

    editar artigos

--- app.py
+++ app.py
@@ -9,14 +9,36 @@ art = Artigos()
 
 @app.route('/inserirA', methods=['GET', 'POST'])
 def inserirA():
+    erro = None
     if request.method == 'POST':
         v1 = request.form['category']
         v2 = request.form['brand']
         v3 = request.form['description']
         v4 = request.form['price']
         art.inserirA(v1, v2, v3, v4)
-    erro = "Artigo inserido com sucesso"
+        erro = "Artigo inserido com sucesso"
     return render_template('Artigos/inserirA.html', erro=erro, usr=usr, art=art)
+
+
+@app.route('/editarA', methods=['GET', 'POST'])
+def editarA():
+    erro = None
+    if request.method == 'POST':
+        if art.id:
+            if "cancel" in request.form:
+                art.reset()
+            elif "delete" in request.form:
+                art.apaga(art.id)
+                erro = "Artigo eliminado com sucesso"
+            elif "edit" in request.form:
+                v1 = request.form['price']
+                art.alterar(art.id, v1)
+                art.select(art.id)  # Atualizar os dados na classe
+                erro = "Preço alterado com sucesso"
+        else:
+            v1 = request.form['id']
+            erro = art.select(v1)
+    return render_template('Artigos/editarA.html', erro=erro, usr=usr, art=art)
 
 
 @app.route('/tabela')
@@ -24,10 +46,12 @@ def tabela():
     title = "Lista de Utilizadores"
     return render_template('tabela.html', title=title, tabela=usr.lista, campos=usr.campos, usr=usr)
 
+
 @app.route('/consultarA')
 def consultarA():
     title = "Lista de Artigos"
     return render_template('tabela.html', title=title, tabela=art.lista, campos=art.campos, usr=usr)
+
 
 @app.route('/registo', methods=['GET', 'POST'])
 def route():
